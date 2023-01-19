88609f0bfed6329f965f06468fcc70952ef33019
Frush97 <mitchellstephenfrush@gmail.com>
2023-01-18 18:28:28 -0700

    Practiced Operators

--- ToolBoxPractice.py
+++ ToolBoxPractice.py
@@ -1,1 +1,33 @@
-print('hello world')
+#Mitchell Frush. 1/18/23. Tool Box Practice
+
+#print('hello world')
+
+#Operator Practice
+
+print(3+3)
+
+print(3.5+4.2)
+
+print(10%3)
+
+print(10/3)
+
+print(10//3)
+
+print(5//3)
+
+num=10
+
+num=num+3
+
+num+=3
+
+powerDial=11
+
+if powerDial<10:
+    print('low')
+elif powerDial>=10 and powerDial<12.5:
+    print('Med')
+else:
+    print('High')
+    
